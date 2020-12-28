2cf2d5b4a62e03aa46b94a7ad25c38c0e449ba0d
Peyton Kellogg <pekellogg@hotmail.com>
2020-12-28 19:18:51 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,24 +1,24 @@
 # addition method and calling it with args specified in test
 def addition(num1, num2)
-  p num1.to_i + num2.to_i
+  p num1 + num2
 end
 addition(5,4)
 
 # subtraction method and calling it with args specified in test
 def subtraction(num1, num2)
-  p num1.to_i - num2.to_i
+  p num1 - num2
 end
 subtraction(10,5)
 
 # division method and calling it with args specified in test
 def division(num1, num2)
-  p num1.to_i / num2.to_i
+  p num1 / num2
 end
 division(50,2)
 
 # multiplication method and calling it with args specified in test
 def multiplication(num1, num2)
-  p num1.to_i * num2.to_i
+  p num1 * num2
 end
 multiplication(4,30)
 
