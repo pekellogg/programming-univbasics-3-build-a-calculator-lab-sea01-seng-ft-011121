91b7884efaecee2aefa49f2c626bb97557bb3cf0
Peyton Kellogg <pekellogg@hotmail.com>
2020-12-28 19:19:12 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,34 @@
+# addition method and calling it with args specified in test
 def addition(num1, num2)
-
+  p num1 + num2
 end
+addition(5,4)
 
+# subtraction method and calling it with args specified in test
 def subtraction(num1, num2)
-
+  p num1 - num2
 end
+subtraction(10,5)
 
+# division method and calling it with args specified in test
 def division(num1, num2)
-
+  p num1 / num2
 end
+division(50,2)
 
+# multiplication method and calling it with args specified in test
 def multiplication(num1, num2)
-
+  p num1 * num2
 end
+multiplication(4,30)
 
+# mod method and calling it with args specified in test
 def modulo(num1, num2)
-
+  p num1 % num2
 end
+modulo(34,5)
 
 def square_root(num)
-
+  p Math.sqrt(num)
 end
+square_root(81)
