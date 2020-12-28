fc4e80141e534b912690d26d799d8fe5d2f5e48d
Peyton Kellogg <pekellogg@hotmail.com>
2020-12-28 18:56:50 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,33 @@
 def addition(num1, num2)
-
+  puts num1.to_i + num2.to_i
+  addition(5,4)
 end
-
-def subtraction(num1, num2)
-
-end
-
-def division(num1, num2)
-
-end
-
-def multiplication(num1, num2)
-
-end
-
-def modulo(num1, num2)
-
-end
-
-def square_root(num)
-
-end
+#
+# def subtraction(num1, num2)
+#   num1 - num2
+#   puts 10 - 5
+# end
+# #
+# #
+# def division(num1, num2)
+#   num1 / num2
+#   puts division(50,2)
+# end
+# #
+# #
+# def multiplication(num1, num2)
+#   num1 * num2
+#   puts multiplication(4,30)
+# end
+# #
+# #
+# def modulo(num1, num2)
+#   num1 % num2
+#   puts modulo(34,5)
+# end
+#
+#
+# def square_root(num)
+#
+#   puts square_root(81)
+# end
